62a951f1ffc79b4121e0765073524b0c1bb33130
irfan-za <fanzahran123@gmail.com>
2022-01-02 16:20:56 +0700

    make functionality on med, hard. And make previous score work

--- quiz/script.js
+++ quiz/script.js
@@ -43,6 +43,99 @@ const ezHint = {
   language: 'a system of communication used by a particular country or community.',
   burning: 'very hot or bright.'
 }
+
+const medQuestions = {
+  courtesy: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/courtesy--_gb_1.mp3',
+  influence: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/influence--_gb_1.mp3',
+  exhibit: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/exhibit--_gb_1.mp3',
+  chrysanthemum: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/chrysanthemum--_gb_1.mp3',
+  bureau: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/bureau--_gb_1.mp3',
+  havoc: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/havoc--_gb_1.mp3',
+  foreknowledge: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/foreknowledge--_gb_1.mp3',
+  scrimmage: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/scrimmage--_gb_1.mp3',
+  polytheism: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/polytheism--_gb_1.mp3',
+  decathlon: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/decathlon--_gb_1.mp3',
+  parallel: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/parallel--_gb_1.mp3',
+  exercise: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/exercise--_gb_1.mp3',
+  slippery: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/slippery--_gb_1.mp3',
+  muffin: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/muffin--_gb_1.mp3',
+  management: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/management--_gb_1.mp3',
+  scenery: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/scenery--_gb_1.mp3',
+  slumber: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/slumber--_gb_1.mp3',
+  spacious: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/spacious--_gb_1.mp3',
+  unknown: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/unknown--_gb_1.mp3',
+  guarantee: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/guarantee--_gb_1.mp3'
+}
+
+const medHint = {
+  courtesy : 'a polite speech or action, especially one required by convention.',
+  influence : 'the power to have an important effect on someone or something',
+  exhibit : 'something presented formally and in a public setting',
+  chrysanthemum : 'any of numerous perennial Old World herbs having showy brightly colored flower heads of the genera',
+  bureau : 'an administrative unit of government',
+  havoc : 'violent and needless disturbance',
+  foreknowledge : 'knowledge of an event before it occurs',
+  scrimmage : '(American football) practice play between a football team\'s squads',
+  polytheism : 'belief in multiple gods',
+  decathlon : 'an athletic contest consisting of ten different events',
+  parallel : 'parallel means two lines that never intersect think of an equal sign',
+  exercise : 'physical activity, like an exercise class, or the act of practicing anything',
+  slippery : 'causing or tending to cause things to slip or slide',
+  muffin : 'a sweet quick bread baked in a cup-shaped pan',
+  management : 'the act of directing or controlling things',
+  scenery : 'a word for how a place looks, especially a beautiful, outdoorsy place',
+  slumber : 'be asleep',
+  spacious : '(of buildings and rooms) having ample space',
+  unknown : 'not known',
+  guarantee : 'an unconditional commitment that something will happen or that something is true',
+}
+
+const hardQuestions = {
+  espionge: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/espionge--_gb_1.mp3',
+  hazardous: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/hazardous--_gb_1.mp3',
+  facsimile: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/facsimile--_gb_1.mp3',
+  magnanimous: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/magnanimous--_gb_1.mp3',
+  nonchalance: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/nonchalance--_gb_1.mp3',
+  onomatopoeia: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/onomatopoeia--_gb_1.mp3',
+  pandemonium: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/pandemonium--_gb_1.mp3',
+  philanthropist: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/philanthropist--_gb_1.mp3',
+  feasibility: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/feasibility--_gb_1.mp3',
+  handkerchief: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/handkerchief--_gb_1.mp3',
+  incandescent: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/incandescent--_gb_1.mp3',
+  blasphemous: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/blasphemous--_gb_1.mp3',
+  expediency: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/expediency--_gb_1.mp3',
+  irreconcilable: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/irreconcilable--_gb_1.mp3',
+  nausea: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/nausea--_gb_1.mp3',
+  notoriety: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/notoriety--_gb_1.mp3',
+  omnipotent: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/omnipotent--_gb_1.mp3',
+  patriarch: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/patriarch--_gb_1.mp3',
+  idiosyncrasy: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/idiosyncrasy--_gb_1.mp3',
+  fallacy: 'https://ssl.gstatic.com/dictionary/static/sounds/oxford/fallacy--_gb_1.mp3'
+}
+
+const hardHint = {
+  espionage :  'the systematic use of spies to get military or political secrets',
+  hazardous : 'involving risk or danger',
+  facsimile :  'a copy or reproduction of something',
+  magnanimous : 'person has a generous spirit',
+  nonchalance :  'a casual lack of concern, a relaxed state without anxiety or enthusiasm',
+  onomatopoeia : 'using words that imitate the sound they denote',
+  pandemonium : 'a state of extreme confusion and disorder',
+  philanthropist : 'a person who gives money or gifts to charities, or helps needy people in other ways',
+  feasibility : 'the quality of being doable',
+  handkerchief : 'a square piece of cloth used for wiping the eyes or nose or as a costume accessory',
+  incandescent: 'emitting light as a result of being heated.',
+  blasphemous: 'sacrilegious against God or sacred things; profane.',
+  expediency: 'the quality of being convenient and practical despite possibly being improper.',
+  irreconcilable: 'any of two or more ideas, facts, or statements that cannot be made compatible.',
+  nausea: 'a feeling of sickness with an inclination to vomit.',
+  notoriety: 'the state of being famous or well known for some bad quality or deed.',
+  omnipotent: 'having unlimited power; able to do anything.',
+  patriarch: 'the male head of a family or tribe.',
+  idiosyncrasy: 'a mode of behaviour or way of thought peculiar to an individual.',
+  fallacy:  'mistaken belief, especially one based on unsound argument.'
+}
+
 // Initializing element
 const wrapper = document.getElementById('wrapper')
 const main = document.getElementById('main')
@@ -53,6 +146,10 @@ const hintWrapper = document.getElementById('hintWrapper')
 const hintBtn = document.getElementById('hint')
 const hint = document.getElementById('showHint')
 const scoreInfo = document.getElementById('scoreInfo')
+// table score
+const ezTable = document.querySelector("#ezScore")
+const medTable = document.querySelector("#medScore")
+const hardTable = document.querySelector("#hardScore")
 let isLookAtHint = false
 
 // init variable
@@ -69,7 +166,7 @@ const answered = []
 wrapper.removeChild(main)
 
 // For timer
-let time = 5 // You can change the time here
+let time = 1 // You can change the time here
 let timeInMinutes = time * 60
 let interval = null
 
@@ -86,6 +183,7 @@ const startTimer = () => {
   timer.innerText = `${minutes < 10 ? '0'+minutes : minutes}:${seconds < 10 ? '0'+seconds : seconds}`
 
   if (timeInMinutes < 0) {
+    finishQuiz()
     clearInterval(interval)
   }
 }
@@ -101,6 +199,28 @@ const handleEasyBtn = () => {
   runQuiz()
   interval = setInterval(startTimer, 1000)
 }
+/**
+ * Handling med button click
+ */
+const handleMedBtn = () => {
+  wrapper.removeChild(difficulty)
+  wrapper.append(main)
+  diff = 'med'
+
+  runQuiz()
+  interval = setInterval(startTimer, 1000)
+}
+/**
+ * Handling hard button click
+ */
+const handleHardBtn = () => {
+  wrapper.removeChild(difficulty)
+  wrapper.append(main)
+  diff = 'hard'
+
+  runQuiz()
+  interval = setInterval(startTimer, 1000)
+}
 
 /**
  * Handling show hint
@@ -110,6 +230,12 @@ const showHint = () => {
   hintWrapper.removeChild(hintBtn)
   switch(diff) {
     case 'easy': hint.innerHTML = `<b>Hint:</b> ${ezHint[currentQuestion]}`
+  }
+  switch(diff) {
+    case 'med': hint.innerHTML = `<b>Hint:</b> ${medHint[currentQuestion]}`
+  }
+  switch(diff) {
+    case 'hard': hint.innerHTML = `<b>Hint:</b> ${hardHint[currentQuestion]}`
   }
 }
 
@@ -122,6 +248,16 @@ const runQuiz = () => {
   switch(diff) {
     case 'easy': {
       currentQuestion = keys(ezQuestions)[question]
+    }
+  }
+  switch(diff) {
+    case 'med': {
+      currentQuestion = keys(medQuestions)[question]
+    }
+  }
+  switch(diff) {
+    case 'hard': {
+      currentQuestion = keys(hardQuestions)[question]
     }
   }
 }
@@ -149,6 +285,16 @@ const getDifficultyQuestion = () => {
       audio = new Audio(values(ezQuestions)[idx])
     }
   }
+  switch(diff) {
+    case 'med': {
+      audio = new Audio(values(medQuestions)[idx])
+    }
+  }
+  switch(diff) {
+    case 'hard': {
+      audio = new Audio(values(hardQuestions)[idx])
+    }
+  }
 
   return idx
 }
@@ -168,6 +314,25 @@ const handleChange = (e) => {
   answers = e.target.value
 }
 
+// finishing quiz
+const finishQuiz = () => {
+  setTimeout(() => {
+    location.reload()
+  }, 3900)
+  alert(`Good job!\nYour score is ${score}\nThankyou for playing :)`)
+
+  switch(diff) { // Insert the score to local storage
+    case 'easy':
+      localStorage.setItem('ezScore', score)
+      break
+    case 'med':
+      localStorage.setItem('medScore', score)
+      break
+    case 'hard':
+      localStorage.setItem('hardScore', score)
+      break
+  }
+}
 /**
  * Handling submit
  * @param {Event} e
@@ -178,7 +343,8 @@ const handleSubmit = (e) => {
     if (answers.toLowerCase() === currentQuestion.toLowerCase()) {
       alert('Bener cuy')
       score += 10
-    } else {
+    } 
+    else {
       alert('Salah bro')
     }
 
@@ -195,3 +361,8 @@ const handleSubmit = (e) => {
     console.log(page, `Hasil score: ${score}`)
   }
 }
+
+// set Table score value
+ezTable.innerHTML=localStorage.getItem("ezScore")
+medTable.innerHTML=localStorage.getItem("medScore")
+hardTable.innerHTML=localStorage.getItem("hardScore")
