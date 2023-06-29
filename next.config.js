3148419a30fb6f0a3b1c63da58a4edfb4ad353f9
irfan-za <fanzahran123@gmail.com>
2023-06-29 09:48:27 +0700

    feat: update styling ui

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    API_URL : 'http://localhost:8000/api/questions',
+  },
+}
 
 module.exports = nextConfig
