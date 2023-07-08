aaa570f966bdd9ff1be689c8c0a5b018ae25e315
irfan-za <fanzahran123@gmail.com>
2023-07-08 20:36:50 +0700

    fix: delete unused auth files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-warning shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Admin
